05a861d6d00432d9b244b21a2e48a8f4ed78ab34
theArtist9 <teja.saiyenprince@gmail.com>
2018-11-11 12:00:23 +0530

    handled string elements

--- recursiveParse.js
+++ recursiveParse.js
@@ -1,37 +1,48 @@
 function isDigit (c) {
     return ("1234567890".indexOf(c) > -1);
 }
-function parseElement(str, i) {
+function parseElement(str, i, type) {
     let e = "";
-    while(str[i] != "," && str[i] != "]" && str[i] != "}") {
+    let arrayElement;
+    while(str[i] != "," && str[i] != "]" && str[i] != "}" && str[i]!="'") {
         e += str[i];
         i++;
     }
+    switch (type) {
+        case "number" :
+            arrayElement = parseInt (e, 10);
+            break;
+        case "string" :
+            arrayElement = e;
+            i++;
+            break;
+        default :
+            let typeException = (type) ? "element type not found" : "type is undefined";
+            throw typeException;
+    }
     return {
-        arrayElement: parseInt(e,10),
-        index : i
+        "arrayElement": arrayElement,
+        "index" : i
     };
 }
 
-
 function arrayParse (str, i) {
-    i = i || 1;
     let res = [];
     let obj = {}
     while (str && i < str.length) {
         c = str[i];
         if (c === "]") {
-            console.log(res);
+            // console.log(res);
             return {
                 arrayElement: res,
                 index: i+1
             };
         }
-        else if (c===",") {
+        else if (c === ",") {
             i++;
         }
         else if (isDigit(c)) {
-            obj = parseElement(str, i);
+            obj = parseElement(str, i , "number");
             res.push(obj.arrayElement);
             i = obj.index;
         }
@@ -40,17 +51,22 @@ function arrayParse (str, i) {
             res.push(obj.arrayElement);
             i = obj.index;
         }
+        else if (c === "'") {
+            obj = parseElement(str, i+1, "string");
+            res.push(obj.arrayElement);
+            i = obj.index;
+        }
         else {
-            let {arrayElement, rest} = parseElement(str.substring(i), "string");
-            str = rest;
-            res.push(arrayElement);
+            throw "Wrong syntax for array"
         }
     }
     
+    throw "Syntax Error";
 }
 
 let arrayExamples = [
-    "[[12,13,[1,2,3,[4],5]],1]"
+    "[[12,13,[1,2,3,[4],5]],1]",
+    "[1,['hello',35,'37',34,5]]]"
 ];
 
 arrayExamples.forEach(a=>console.log(JSON.stringify(arrayParse(a,1))));
